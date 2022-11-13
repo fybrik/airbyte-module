c1fea6509eb24b65e436351729b6482386447333
Doron Chen <cdoron@il.ibm.com>
2022-11-13 17:32:51 +0200

    update wait for pod to be ready
    Signed-off-by: Doron Chen <cdoron@il.ibm.com>

--- helm/client/deploy_airbyte_module_client_pod.sh
+++ helm/client/deploy_airbyte_module_client_pod.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-kubectl run my-shell --image ghcr.io/fybrik/airbyte-module-client:main --image-pull-policy=Always -n default --wait
-kubectl wait pod --for=condition=ready my-shell -n default
+kubectl run my-shell --image ghcr.io/fybrik/airbyte-module-client:main --image-pull-policy=Always -n default
+kubectl wait pod --for=condition=ready my-shell -n default --timeout 10m
